3352c966dce5b871f792a594ce9da25d72c9b170
Suneel-Ahmed <suneelahmed413@gmail.com>
2025-01-02 01:38:48 +0500

    bozecoin main

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen bg-yellow-300 flex justify-center items-center' >
+      <h1 className='text-[90px] text-purple-800' >Boze Coin</h1>
+    </div>
+  )
+}
+
+export default App
