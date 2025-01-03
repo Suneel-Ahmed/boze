89cb7c2cb67731547d850053a8d154ea7c561f7c
Suneel-Ahmed <suneelahmed413@gmail.com>
2025-01-04 02:04:11 +0500

    add changes

--- src/components/HomeSec2.jsx
+++ src/components/HomeSec2.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HomeSec2 = () => {
   return (
-    <div className='w-full h-screen bg-red-500 flex justify-center items-center' >
-        <h1 className='text-[60px]' >Hello</h1>
+    <div className='w-full min-h-screen py-[70px]  flex justify-center items-center' >
+        <img src="/images/mobile/mobile.png " className='w-[400px]' alt="mobile" />
     </div>
   )
 }
