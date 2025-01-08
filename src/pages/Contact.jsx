c4166301f0a0ade308fd9568bcb3c21f909dae4d
Suneel-Ahmed <suneelahmed413@gmail.com>
2025-01-09 03:37:52 +0500

    add changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
+import { IoSend } from "react-icons/io5";   
 
 const Contact = () => {
   return (
-    <div className='w-full h-screen flex-col gap-[30px] flex justify-center items-center' >
-                    <h1 className='text-white text-[50px] ' >Hello I am Support Page</h1>
-        <div className='w-[70%] min-h-fit py-[100px] px-[100px] bg-white/10 rounded-3xl glass' >
+    <div className='w-full h-[80vh] flex-col gap-[30px] flex justify-center items-center' >
+                    <h1 className='text-white text-[clamp(16px,2.6vw,50px)] uppercase quicksand ' >Contact Us</h1>
+    <div className='w-[70%] min-h-fit py-[50px]  px-[clamp(20px,5.3vw,100px)] bg-white/10 rounded-3xl glass' >
                 <form action="">
-                    <div className='flex flex-col gap-4' >
+                    <div className='flex  flex-col gap-4' >
                         <label htmlFor="" className='text-white text-[20px]'>Name</label>
-                        <input type="text" className='px-[10px] py-3 rounded-xl border bg-transparent '  placeholder='Enter Your Name' />
+                        <input type="text" className='px-[10px] text-white py-3 rounded-xl border bg-transparent '  placeholder='Enter Your Name' />
                     </div>
                     <div className='flex mt-3 flex-col gap-4' >
                         <label htmlFor="" className='text-white text-[20px]'>Email</label>
-                        <input type="text" className='px-[10px] py-3 rounded-xl border bg-transparent '  placeholder='Enter Your Email' />
+                        <input type="text" className='px-[10px] text-white py-3 rounded-xl border bg-transparent '  placeholder='Enter Your Email' />
                     </div>
                     <div className='flex mt-3 flex-col gap-4' >
                         <label htmlFor="" className='text-white text-[20px]'>Message</label>
-                        <input type="text" className='px-[10px] py-3 rounded-xl border bg-transparent '  placeholder='Enter Your Name' />
+                        <textarea type="text" rows={4} className='px-[10px] text-white py-3 rounded-xl border bg-transparent '  placeholder='Write Your Message' />
+                    </div>
+                    <div className='w-full flex mt-5 justify-end' >
+                        <button className='btn ' >Send <span><IoSend /></span> </button>
                     </div>
                 </form>
         </div>
