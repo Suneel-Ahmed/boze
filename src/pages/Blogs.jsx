fcfc7455bac0af55c167ca414acd63ee5697741c
Suneel-Ahmed <suneelahmed413@gmail.com>
2025-01-20 02:49:41 +0500

    fix click scroll to top

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -24,6 +24,9 @@ const Blogs = () => {
   },[])
 
  
+  const handleClick = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
 
   return (
     <>
@@ -38,11 +41,11 @@ const Blogs = () => {
 
             // /blogs/:id
           <div key={val?.id} className='max-w-[1200px]  rounded-xl  w-full h-auto  max-lg:w-full  flex flex-col gap-[clamp(10px,2.3vw,30px)] ' >
-                <Link to={`/blogs/${val?.slug}`} className='max-w-[1200px] max-h-[630px] mx-auto   rounded-2xl overflow-hidden' >
+                <Link onClick={handleClick} to={`/blogs/${val?.slug}`} className='max-w-[1200px] max-h-[630px] mx-auto   rounded-2xl overflow-hidden' >
                 <img src={`${import.meta.env.VITE_API_URL}/${val?.cover_img}`} alt={val?.cover_img} className=' w-full h-full object-cover object-center' />
                 </Link>
                 <div className='w-full flex flex-col items-center justify-center '>
-                 <Link to={`/blogs/${val?.slug}`} > <h1 className='text-white text-[clamp(20px,5.2vw,50px)] mb-10' >{val?.title}</h1></Link>
+                 <Link onClick={handleClick} to={`/blogs/${val?.slug}`} > <h1 className='text-white text-[clamp(20px,5.2vw,50px)] mb-10' >{val?.title}</h1></Link>
                   <div className='w-full h-[1px] bg-white/40 max-w-[1200px] rounded-3xl max-md:mt-0 mt-auto' ></div>
                 </div>
           </div>
